209ae252543d34e03ec4cec4ca02a2c7114b556b
Andrew Ebert <a>
2014-07-12 23:11:01 +0930

    Blew away old repo

--- make.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-cat templates/map.html | sed -e 's/\.\.\/static/static/g' \
-    -e 's/static\/js\/lib\/angular\.js/https:\/\/ajax.googleapis.com\/ajax\/libs\/angularjs\/1.1.4\/angular.min.js/' > index.html
